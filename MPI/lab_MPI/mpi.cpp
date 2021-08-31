6f74a1897110adbbc10535f1c7af60c967a6b383
mkrivets <mkrivets1@gmail.com>
2021-08-31 06:51:12 +0300

    fixed algorithms

--- MPI/lab_MPI/mpi.cpp
+++ MPI/lab_MPI/mpi.cpp
@@ -1,60 +1,74 @@
 #include <stdio.h>
 #include <time.h>
 #include "mpi.h"
+#include <vector>
 
 
 int* get_interval(int, int, int*);
 inline void print_palin_range(int, int);
+std::vector< int > palindrom_vector;
+
 
 int main(int argc, char** argv)
 {
 	int thread, thread_size;
-	int* thread_range, interval[] = { 1, 10000 };
-	double cpu_time_start, cpu_time_fini;
+	int* thread_range, interval[] = { 1, 1000 };
+	double cpu_time_start, cpu_time_fin, res_time;
 
 	MPI_Status status;
 
+	// init the MPI execution environment
 	MPI_Init(&argc, &argv);
-
-	// Отримуємо номер конкретного процесу на якому запущена програма
+	// get the rank (num) of running process 
 	MPI_Comm_rank(MPI_COMM_WORLD, &thread);
-
-	// Отримуємо кількість запущених процесів
+	// get the amount of processes
 	MPI_Comm_size(MPI_COMM_WORLD, &thread_size);
 
-	if (thread == 0)
-	{
-		// Відправляємо кожному процесу інтервал
-		for (int to_thread = 1; to_thread < thread_size; to_thread++)
-			MPI_Send(&interval, 2, MPI_INT, to_thread, 0, MPI_COMM_WORLD);
+	if (thread == 0) {
+		// send interval for each process
+		for (int thread_num = 1; thread_num < thread_size; thread_num++)
+			// send buffer, num of elements in send buffer, type of that elements,
+			// process rank, message tag, communicator
+			MPI_Send(&interval, 2, MPI_INT, thread_num, 0, MPI_COMM_WORLD);
 
 		cpu_time_start = MPI_Wtime();
 	}
-	// Якщо процес не перший - чекамо дані
+	// otherwise blocking receive for a message
 	else
+		// address of receive buffer, max num of elements in receive buffer, type, rank of source,
+		// message tag, communicator
 		MPI_Recv(&interval, 2, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 
+	// get the range for current thread
 	thread_range = get_interval(thread, thread_size, interval);
 
+	// get all palindroms from this range
 	print_palin_range(thread_range[0], thread_range[1]);
 
+	// fin of processes
 	if (thread == 0)
 	{
-		cpu_time_fini = MPI_Wtime();
-		printf("CPU Time: %lf ms\n", (cpu_time_fini - cpu_time_start) * 1000);
+		cpu_time_fin = MPI_Wtime();
+		// print results
+		res_time = cpu_time_fin - cpu_time_start;
+		printf("CPU Time: %lf ms\n", res_time * 1000);
 	}
 
 	MPI_Finalize();
 	return 0;
 }
 
+// gets the num of process, amount of all process, arr with interval
+// returns range for certain process
 int* get_interval(int proc, int size, int* interval)
 {
 	int* range = new int[2];
 	int interval_size = (interval[1] - interval[0]) / size;
 
+	// get the beginning and the end of range for proc
 	range[0] = interval[0] + interval_size * proc;
 	range[1] = interval[0] + interval_size * (proc + 1);
+	// in case of the last process
 	range[1] = range[1] == interval[1] - 1 ? interval[1] : range[1];
 	return range;
 }
@@ -64,13 +78,16 @@ inline void print_palin_range(int ibeg, int iend)
 	for (int i = ibeg; i <= iend; i++)
 	{
 		int digit, rev = 0, num = i;
+		// get the rev - reversed of i
 		do
 		{
 			digit = num % 10;
 			rev = (rev * 10) + digit;
 			num = num / 10;
 		} while (num != 0);
-		if (rev == i) printf("Palindrom value ---> %d\n", i);
+		// if reversed is base i -> palindrom
 
+		// if (rev == i) printf("Palindrom - %d\n", i);
+		if (rev == i) palindrom_vector.push_back(i);
 	}
 }
--- Sequential/lab_Seq/sequential.cpp
+++ Sequential/lab_Seq/sequential.cpp
@@ -2,31 +2,40 @@
 #include <stdlib.h>
 #include <iostream>
 #include <omp.h>
+#include <ctime>
+#include <vector>
 
 using namespace std;
+std::vector<int> palindrom_vector;
 
 bool isPalindrome(int x) {
     if (x < 0)
         return false;
-    int div = 1;
-    while (x / div >= 10) {
-        div *= 10;
-    }
-    while (x != 0) {
-        int l = x / div;
-        int r = x % 10;
-        if (l != r)
-            return false;
-        x = (x % div) / 10;
-        div /= 100;
-    }
-    return true;
+    int digit, rev = 0, num = x;
+    // get the rev - reversed of x
+    do
+    {
+        digit = num % 10;
+        rev = (rev * 10) + digit;
+        num = num / 10;
+    } while (num != 0);
+    // if reversed is base x -> palindrom
+    if (rev == x) return true;
+    else return false;
 }
 
 int main(int argc, char* argv[])
 {
-    int a = 1, b = 10000;
+    int a = 1, b = 1000;
+    double res_time, timein, timeout;
+    timein = omp_get_wtime();
+    
     for (int n = a; n < b; n++)
-        if (isPalindrome(n)) printf("Palindrom value ---> %d\n", n);
+        if (isPalindrome(n)) printf("Palindrom - %d\n", n);
+        // if (isPalindrome(n)) palindrom_vector.push_back(n);
+
+    timeout = omp_get_wtime();
+    res_time = timeout - timein;
+    printf("CPU Time: %lf ms\n", res_time);
     return 0;
 }
